2248afd407cc716aaa2ad5146bff47a78117fd88
nishantHolla <nishantholla19@gmail.com>
2024-11-01 19:15:28 +0530

    Remove theme switcher

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
     extend: {
+      animation: {
+        text: "text 10s ease infinite alternate",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
       colors: {
         background: "var(--color-background)",
-        content: "var(--color-content)"
-      }
+        content: "var(--color-content)",
+      },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
